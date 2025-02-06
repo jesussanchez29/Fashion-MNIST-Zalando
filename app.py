ef208062086f3a9a358baf3edb0eea143fd86905
jesussanchez29 <74043073+jesussanchez29@users.noreply.github.com>
2025-02-06 09:41:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 import numpy as np
 
 # Cargar el modelo
-model = load_model('/content/drive/MyDrive/CURSO DE IA Y BIG DATA/PIA/REDES NEURONALES/models/fashion_mnist.keras')
+model = load_model('fashion_mnist.keras')
 
 # Crear la interfaz de usuario
 st.title("Clasificador Fashion MNIST")
